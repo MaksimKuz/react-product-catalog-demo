b1c457fd8e87531560f81089ff040c0c07c321f9
MaksimKuznetsov <maxim.kuznecov@galaktika.ru>
2025-05-20 21:26:53 +0300

    Переименование

--- src/ToDoListDeleteConfirmation.jsx
+++ src/ToDoListDeleteConfirmation.jsx
@@ -6,19 +6,19 @@ export  default function ToDoListDeleteConfirmation({title, onConfirm, onHide}){
 
     const [showDialog, setShowDialog] = useState(true);
 
-    const hideDeleteProductsDialog = () => {
+    const hideDialog = () => {
         setShowDialog(false); onHide()
     };
 
     const deleteProductsDialogFooter = (
         <>
             <Button label="Да" icon="pi pi-check" severity="danger" onClick={onConfirm} />
-            <Button label="Нет" icon="pi pi-times" outlined onClick={hideDeleteProductsDialog} />
+            <Button label="Нет" icon="pi pi-times" outlined onClick={hideDialog} />
         </>
     );
     return(
         <Dialog visible={showDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }}
-                header="Подтверждение" modal footer={deleteProductsDialogFooter} onHide={hideDeleteProductsDialog}>
+                header="Подтверждение" modal footer={deleteProductsDialogFooter} onHide={hideDialog}>
         <div className="confirmation-content">
             <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
             <span>{title}</span>
