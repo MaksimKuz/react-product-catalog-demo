69a09ba77f2dbca1f27273fe07ad2c7ca953dd6c
MaksimKuznetsov <maxim.kuznecov@galaktika.ru>
2025-05-20 21:26:24 +0300

    Реализовано добавление задачи. Окно добавления вынесено в компонент

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -7,13 +7,9 @@ import {InputIcon} from "primereact/inputicon";
 import {InputText} from "primereact/inputtext";
 import React, {useRef, useState} from "react";
 import {Toolbar} from "primereact/toolbar";
-import {Dialog} from "primereact/dialog";
 import ToDoListDeleteConfirmation from "./ToDoListDeleteConfirmation.jsx";
 import {Toast} from "primereact/toast";
-import {classNames} from "primereact/utils";
-import {InputTextarea} from "primereact/inputtextarea";
-import {RadioButton} from "primereact/radiobutton";
-import {InputNumber} from "primereact/inputnumber";
+import ToDoListAdd from "./ToDoListAdd.js";
 
 export default function ToDoList() {
 
@@ -34,7 +30,7 @@ export default function ToDoList() {
     const leftToolbarTemplate = () => {
         return (
             <div className="flex flex-wrap gap-2">
-                <Button label="Добавить" icon="pi pi-plus" severity="success" onClick={openNew} />
+                <Button label="Добавить" icon="pi pi-plus" severity="success" onClick={()=> setShowAddProductDialog(true)} />
                 <Button label="Удалить" icon="pi pi-trash" severity="danger"
                         onClick={()=> setShowDeleteProductsDialog(true)} disabled={!selectedProducts || selectedProducts.length === 0}  />
             </div>
@@ -46,29 +42,56 @@ export default function ToDoList() {
     };
     //endregion
 
-    let newTask = {
-        id: null,
-        name: '',
-        image: null,
-        description: '',
-        category: null,
-        price: 0,
-        quantity: 0,
-        rating: 0,
-        inventoryStatus: 'INSTOCK'
-    };
-
     const [products, setProducts] = useState(getProducts());
-    const [product, setProduct] = useState(newTask);
-    const [productDialog, setProductDialog] = useState(false);
-    const [submitted, setSubmitted] = useState(false);
+    const [showAddProductDialog, setShowAddProductDialog] = useState(false);
 
-    const openNew = () => {
-        setProduct(newTask);
-        setSubmitted(false);
-        setProductDialog(true);
+   const toast = useRef(null);
+
+    //region Создание элементов
+    const addNewProduct = (product) => {
+        setShowAddProductDialog(false);
+
+        let _products = [...products];
+        let _product = { ...product };
+
+        if (product.id) {
+            const index = findIndexById(product.id);
+
+            _products[index] = _product;
+            toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
+        } else {
+            _product.id = createId();
+            _product.image = 'product-placeholder.svg';
+            _products.push(_product);
+            toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
+        }
+
+        setProducts(_products);
     };
-    const toast = useRef(null);
+
+    const findIndexById = (id) => {
+        let index = -1;
+
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    };
+
+    const createId = () => {
+        let id = '';
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (let i = 0; i < 5; i++) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        return id;
+    };
+    //endregion
 
     //region Удаление элементов
     const [showDeleteProductsDialog, setShowDeleteProductsDialog] = useState(false);
@@ -88,69 +111,6 @@ export default function ToDoList() {
         dt.current.exportCSV();
     };
     //endregion
-
-    const onCategoryChange = (e) => {
-        let _product = { ...product };
-
-        _product['category'] = e.value;
-        setProduct(_product);
-    };
-
-    const onInputChange = (e, name) => {
-        const val = (e.target && e.target.value) || '';
-        let _product = { ...product };
-
-        _product[`${name}`] = val;
-
-        setProduct(_product);
-    };
-
-    const onInputNumberChange = (e, name) => {
-        const val = e.value || 0;
-        let _product = { ...product };
-
-        _product[`${name}`] = val;
-
-        setProduct(_product);
-    };
-
-    const hideDialog = () => {
-        setSubmitted(false);
-        setProductDialog(false);
-    };
-
-
-    const saveProduct = () => {
-        // setSubmitted(true);
-        //
-        // if (product.name.trim()) {
-        //     let _products = [...products];
-        //     let _product = { ...product };
-        //
-        //     if (product.id) {
-        //         const index = findIndexById(product.id);
-        //
-        //         _products[index] = _product;
-        //         toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-        //     } else {
-        //         _product.id = createId();
-        //         _product.image = 'product-placeholder.svg';
-        //         _products.push(_product);
-        //         toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-        //     }
-        //
-        //     setProducts(_products);
-        //     setProductDialog(false);
-        //     setProduct(emptyProduct);
-        // }
-    };
-
-    const productDialogFooter = (
-        <React.Fragment>
-            <Button label="Отменить" icon="pi pi-times" outlined onClick={hideDialog} />
-            <Button label="Сохранить" icon="pi pi-check" onClick={saveProduct} />
-        </React.Fragment>
-    );
 
     // Собственно разметка таблицы
     return (
@@ -172,63 +132,8 @@ export default function ToDoList() {
                 </DataTable>
             </div>
 
-            <Dialog
-                visible={productDialog}
-                    style={{ width: '32rem' }}
-                    breakpoints={{ '960px': '75vw', '641px': '90vw' }}
-                    header="Сведения о задаче" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
-                {product.image && <img src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.image} className="product-image block m-auto pb-3" />}
-                <div className="field">
-                    <label htmlFor="name" className="font-bold">
-                        Название
-                    </label>
-                    <InputText id="name" value={product.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product.name })} />
-                    {submitted && !product.name && <small className="p-error">Name is required.</small>}
-                </div>
-                <div className="field">
-                    <label htmlFor="description" className="font-bold">
-                        Описание
-                    </label>
-                    <InputTextarea id="description" value={product.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
-                </div>
-
-                <div className="field">
-                    <label className="mb-3 font-bold">Категория</label>
-                    <div className="formgrid grid">
-                        <div className="field-radiobutton col-6">
-                            <RadioButton inputId="category1" name="category" value="Accessories" onChange={onCategoryChange} checked={product.category === 'Accessories'} />
-                            <label htmlFor="category1">Accessories</label>
-                        </div>
-                        <div className="field-radiobutton col-6">
-                            <RadioButton inputId="category2" name="category" value="Clothing" onChange={onCategoryChange} checked={product.category === 'Clothing'} />
-                            <label htmlFor="category2">Clothing</label>
-                        </div>
-                        <div className="field-radiobutton col-6">
-                            <RadioButton inputId="category3" name="category" value="Electronics" onChange={onCategoryChange} checked={product.category === 'Electronics'} />
-                            <label htmlFor="category3">Electronics</label>
-                        </div>
-                        <div className="field-radiobutton col-6">
-                            <RadioButton inputId="category4" name="category" value="Fitness" onChange={onCategoryChange} checked={product.category === 'Fitness'} />
-                            <label htmlFor="category4">Fitness</label>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="formgrid grid">
-                    <div className="field col">
-                        <label htmlFor="price" className="font-bold">
-                            Price
-                        </label>
-                        <InputNumber id="price" value={product.price} onValueChange={(e) => onInputNumberChange(e, 'price')} mode="currency" currency="USD" locale="en-US" />
-                    </div>
-                    <div className="field col">
-                        <label htmlFor="quantity" className="font-bold">
-                            Приоритет
-                        </label>
-                        <InputNumber id="quantity" value={product.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} />
-                    </div>
-                </div>
-            </Dialog>
+            {showAddProductDialog && <ToDoListAdd
+                onHide={() => setShowAddProductDialog(false)} onSave={(product)=> addNewProduct(product)}/>}
 
             {showDeleteProductsDialog && <ToDoListDeleteConfirmation
                 title="Вы уверены, что хотите удалить выбранную задачу?"
---
+++ src/ToDoListAdd.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {Button} from "primereact/button";
+import {InputText} from "primereact/inputtext";
+import {classNames} from "primereact/utils";
+import {InputTextarea} from "primereact/inputtextarea";
+import {RadioButton} from "primereact/radiobutton";
+import {InputNumber} from "primereact/inputnumber";
+import {Dialog} from "primereact/dialog";
+
+export default function ToDoListAdd({onHide, onSave}) {
+
+    let newTask = {
+        id: null,
+        name: '',
+        image: null,
+        description: '',
+        category: 'Accessories',
+        price: 0,
+        quantity: 0,
+        rating: 0,
+        inventoryStatus: 'INSTOCK'
+    };
+    const [showDialog, setShowDialog] = useState(true);
+    const [product, setProduct] = useState(newTask);
+    const [submitted, setSubmitted] = useState(false);
+
+    const hideDialog = () => {
+        setSubmitted(false);
+        setShowDialog(false); onHide();
+    };
+
+    const saveDialog = () => {
+        setSubmitted(true);
+        setShowDialog(false); onSave(product);
+    };
+
+    const onCategoryChange = (e) => {
+        let _product = { ...product };
+
+        _product['category'] = e.value;
+        setProduct(_product);
+    };
+
+    const onInputChange = (e, name) => {
+        const val = (e.target && e.target.value) || '';
+        let _product = { ...product };
+
+        _product[`${name}`] = val;
+
+        setProduct(_product);
+    };
+
+    const onInputNumberChange = (e, name) => {
+        const val = e.value || 0;
+        let _product = { ...product };
+
+        _product[`${name}`] = val;
+
+        setProduct(_product);
+    };
+
+    const dialogFooter = (
+        <>
+            <Button label="Сохранить" icon="pi pi-check" onClick={saveDialog} />
+            <Button label="Отменить" icon="pi pi-times" outlined onClick={hideDialog} />
+        </>
+    );
+
+    return (
+        <>
+            <Dialog visible={showDialog} style={{ width: '32rem' }}
+                    breakpoints={{ '960px': '75vw', '641px': '90vw' }}
+                    header="Сведения о задаче" modal className="p-fluid" footer={dialogFooter}
+                    onHide={hideDialog}>
+
+                {product.image && <img src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`}
+                                       alt={product.image} className="product-image block m-auto pb-3" />}
+
+                <div className="field">
+                    <label htmlFor="name" className="font-bold">
+                        Название
+                    </label>
+                    <InputText id="name" value={product.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product.name })} />
+                    {submitted && !product.name && <small className="p-error">Name is required.</small>}
+                </div>
+                <div className="field">
+                    <label htmlFor="description" className="font-bold">
+                        Описание
+                    </label>
+                    <InputTextarea id="description" value={product.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
+                </div>
+
+                <div className="field">
+                    <label className="mb-3 font-bold">Category</label>
+                    <div className="formgrid grid">
+                        <div className="field-radiobutton col-6">
+                            <RadioButton inputId="category1" name="category" value="Accessories" onChange={onCategoryChange} checked={product.category === 'Accessories'} />
+                            <label htmlFor="category1">Accessories</label>
+                        </div>
+                        <div className="field-radiobutton col-6">
+                            <RadioButton inputId="category2" name="category" value="Clothing" onChange={onCategoryChange} checked={product.category === 'Clothing'} />
+                            <label htmlFor="category2">Clothing</label>
+                        </div>
+                        <div className="field-radiobutton col-6">
+                            <RadioButton inputId="category3" name="category" value="Electronics" onChange={onCategoryChange} checked={product.category === 'Electronics'} />
+                            <label htmlFor="category3">Electronics</label>
+                        </div>
+                        <div className="field-radiobutton col-6">
+                            <RadioButton inputId="category4" name="category" value="Fitness" onChange={onCategoryChange} checked={product.category === 'Fitness'} />
+                            <label htmlFor="category4">Fitness</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="formgrid grid">
+                    <div className="field col">
+                        <label htmlFor="price" className="font-bold">
+                            Price
+                        </label>
+                        <InputNumber id="price" value={product.price} onValueChange={(e) => onInputNumberChange(e, 'price')} mode="currency" currency="USD" locale="en-US" />
+                    </div>
+                    <div className="field col">
+                        <label htmlFor="quantity" className="font-bold">
+                            Quantity
+                        </label>
+                        <InputNumber id="quantity" value={product.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} />
+                    </div>
+                </div>
+
+            </Dialog>
+        </>
+    )
+}
