44486c5db1147d53dfb50ec647a0f353671b309d
MaksimKuznetsov <maxim.kuznecov@galaktika.ru>
2025-06-04 23:00:23 +0300

    Доработка домашней страницы

--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,14 @@
 import {NavLink} from "react-router-dom";
+import Back from "/src/assets/products.jpg"
 
 export default function Home() {
     return (
-        <>
-            <h1>Каталог продуктов</h1>
-            <NavLink to="/products">
-                <h2>Перейти в каталог</h2>
-            </NavLink>
-        </>
+        <div style="position: absolute;top: 0px;left: 0px;width:100vw;height:100vh;display: flex;flex-direction: column;justify-content: center;align-items: center;background-image: url(/src/assets/products.jpg)">
+            <div style="width:40vw;height:15vh;border: 2px solid;border-radius: 10px;background:white;">
+                <h1 class="m-0">Каталог продуктов</h1>
+                <NavLink to="/products">
+                    <h2>Перейти в каталог</h2>
+                </NavLink></div>
+            </div>
     );
 }
