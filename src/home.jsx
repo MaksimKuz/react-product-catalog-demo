4489384a9e9056cdbaf3309e6cded14a9a0d149a
MaksimKuznetsov <maxim.kuznecov@galaktika.ru>
2025-06-04 23:17:16 +0300

    Рефакторинг стилей

--- src/home.jsx
+++ src/home.jsx
@@ -2,13 +2,38 @@ import {NavLink} from "react-router-dom";
 import Back from "/src/assets/products.jpg"
 
 export default function Home() {
+
+    const backgroundBlockStyle = {
+            position: "absolute",
+            top: "0px",
+            left: "0px",
+            width: "100vw",
+            height: "100vh",
+
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+
+            backgroundImage: "url(/src/assets/products.jpg)"
+    }
+
+    const titleBlockStyle = {
+            width: "40vw",
+            height: "15vh",
+            border: "2px solid silver",
+            borderRadius: "10px",
+            background: "white"
+        }
+
     return (
-        <div style="position: absolute;top: 0px;left: 0px;width:100vw;height:100vh;display: flex;flex-direction: column;justify-content: center;align-items: center;background-image: url(/src/assets/products.jpg)">
-            <div style="width:40vw;height:15vh;border: 2px solid;border-radius: 10px;background:white;">
+        <div style={backgroundBlockStyle}>
+            <div style={titleBlockStyle}>
                 <h1 class="m-0">Каталог продуктов</h1>
                 <NavLink to="/products">
                     <h2>Перейти в каталог</h2>
-                </NavLink></div>
+                </NavLink>
             </div>
-    );
+        </div>
+    )
 }
