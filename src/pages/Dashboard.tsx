7f44bbba2e05e14958e0fb181b54301eb03757e2
MaksimKuznetsov <maxim.kuznecov@galaktika.ru>
2025-06-12 16:22:43 +0300

    Вынес список продаваемых продуктов в отдельный компонент MostSaledProducts.jsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,10 @@
-import { Button } from 'primereact/button';
-import { Chart } from 'primereact/chart';
-import { Menu } from 'primereact/menu';
 import { useEffect, useRef, useState } from 'react';
-import { ChartData, ChartOptions } from 'chart.js';
 import {ProductService} from "../services/ProductsService";
 import {Card} from "primereact/card";
 import {appStore} from "../models/AppStore.ts";
 import {observer} from "mobx-react";
 import SalesChart from "../components/SalesChart.tsx";
-
+import MostSaledProducts from "../components/MostSaledProducts.jsx";
 
 const Dashboard = observer(() => {
     const [products, setProducts] = useState<Demo.Product[]>([]);
@@ -89,121 +85,14 @@ const Dashboard = observer(() => {
 
             <div className="col-6" >
                 <Card style={{borderRadius: "15px"}}>
-                <div className="flex justify-content-between align-items-center">
                     <h3>Самые продаваемые продукты</h3>
-                </div>
-                <ul className="list-none p-0 m-0">
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Space T-Shirt</span>
-                            <div className="mt-1 text-600">Clothing</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-orange-500 h-full" style={{width: '50%'}}/>
-                            </div>
-                            <span className="text-orange-500 ml-3 font-medium">%50</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Portal Sticker</span>
-                            <div className="mt-1 text-600">Accessories</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-cyan-500 h-full" style={{width: '16%'}}/>
-                            </div>
-                            <span className="text-cyan-500 ml-3 font-medium">%16</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Portal Sticker</span>
-                            <div className="mt-1 text-600">Accessories</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-cyan-500 h-full" style={{width: '16%'}}/>
-                            </div>
-                            <span className="text-cyan-500 ml-3 font-medium">%16</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Supernova Sticker</span>
-                            <div className="mt-1 text-600">Accessories</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-pink-500 h-full" style={{width: '67%'}}/>
-                            </div>
-                            <span className="text-pink-500 ml-3 font-medium">%67</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Wonders Notebook</span>
-                            <div className="mt-1 text-600">Office</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-green-500 h-full" style={{width: '35%'}}/>
-                            </div>
-                            <span className="text-green-500 ml-3 font-medium">%35</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Mat Black Case</span>
-                            <div className="mt-1 text-600">Accessories</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-purple-500 h-full" style={{width: '75%'}}/>
-                            </div>
-                            <span className="text-purple-500 ml-3 font-medium">%75</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Mat Black Case</span>
-                            <div className="mt-1 text-600">Accessories</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-purple-500 h-full" style={{width: '75%'}}/>
-                            </div>
-                            <span className="text-purple-500 ml-3 font-medium">%75</span>
-                        </div>
-                    </li>
-                    <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-                        <div>
-                            <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Robots T-Shirt</span>
-                            <div className="mt-1 text-600">Clothing</div>
-                        </div>
-                        <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                            <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                 style={{height: '8px'}}>
-                                <div className="bg-teal-500 h-full" style={{width: '40%'}}/>
-                            </div>
-                            <span className="text-teal-500 ml-3 font-medium">%40</span>
-                        </div>
-                    </li>
-                </ul>
+                    <MostSaledProducts/>
             </Card>
             </div>
 
             <div className="col-6" >
                 <Card style={{borderRadius: "15px"}}>
-                <h3>Продажи по месяцам</h3>
+                    <h3>Продажи по месяцам</h3>
                     <SalesChart/>
                 </Card>
             </div>
